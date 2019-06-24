167e4ae2e095531fc1cfbb462a39ca9cfa5e5802
benlalanes <bllanes@ucsd.edu>
2019-06-24 11:34:32 -0700

    Add redis caching.

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	//"encoding/json"
 	"flag"
+
+	//"flag"
 	"fmt"
 	"html/template"
 	"log"
@@ -12,6 +16,7 @@ import (
 	"time"
 
 	"github.com/gophercises/quiet_hn/hn"
+	"github.com/go-redis/redis"
 )
 
 type story struct {
@@ -34,8 +39,59 @@ func (s StorySlice) Len() int {
 	return len(s)
 }
 
+var client *redis.Client
+
+const QuietHnResultsKey = "quiet-hn-results:"
+
+type Info struct {
+	Id int
+	Name string
+}
+
 func main() {
 	// parse flags
+
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	//resultsStr, err := client.Get("quiet-hn-results:").Result()
+	client.Del(QuietHnResultsKey).Result()
+	//if err == redis.Nil {
+	//	fmt.Println("No cache found.")
+	//}
+	//fmt.Printf("%q\n", resultsStr)
+	//fmt.Println(err)
+	//
+	//
+	//infos := []*Info{
+	//	{Id: 0, Name: "Benjamin"},
+	//}
+
+	//info := Info{
+	//	Id: 0,
+	//	Name: "Michele",
+	//}
+
+	//infosStr, err := json.Marshal(infos)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println(string(infosStr))
+	//
+	//var infos2 []Info
+	//
+	//err = json.Unmarshal(infosStr, &infos2)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	//fmt.Printf("%#v\n", infos2)
+
+
 	var port, numStories int
 	flag.IntVar(&port, "port", 3000, "the port to start the web server on")
 	flag.IntVar(&numStories, "num_stories", 30, "the number of top stories to display")
@@ -43,13 +99,13 @@ func main() {
 
 	tpl := template.Must(template.ParseFiles("./index.gohtml"))
 
-	http.HandleFunc("/", handler(numStories, tpl))
+	http.HandleFunc("/", handler(numStories, tpl, client))
 
 	// Start the server
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil))
 }
 
-func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+func handler(numStories int, tpl *template.Template, redisClient *redis.Client) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		var client hn.Client
@@ -60,40 +116,68 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 		}
 
 		var stories []item
-		limit := int(1.25 * float64(numStories))
 
-		ch := make(chan story, len(ids))
+		storiesJson, err := redisClient.Get(QuietHnResultsKey).Result()
+		if err == redis.Nil {
+			limit := int(1.25 * float64(numStories))
 
-		for i := 0; i < limit; i++ {
+			ch := make(chan story, len(ids))
 
-			go func(id int, idx int) {
-				hnItem, err := client.GetItem(id)
-				if err != nil {
-					return
-				}
-				item := parseHNItem(hnItem)
-				if isStoryLink(item) {
+			for i := 0; i < limit; i++ {
 
-					ch <- story{
-						id: id,
-						idx: idx,
-						content: item,
+				go func(id int, idx int) {
+					hnItem, err := client.GetItem(id)
+					if err != nil {
+						return
 					}
-				}
-			}(ids[i], i)
+					item := parseHNItem(hnItem)
+					if isStoryLink(item) {
 
-		}
+						ch <- story{
+							id: id,
+							idx: idx,
+							content: item,
+						}
+					}
+				}(ids[i], i)
 
-		var storiesIndexed StorySlice
+			}
 
-		for i := 0; i < numStories; i++ {
-			storiesIndexed = append(storiesIndexed, <-ch)
-		}
+			var storiesIndexed StorySlice
 
-		sort.Sort(storiesIndexed)
+			for i := 0; i < numStories; i++ {
+				storiesIndexed = append(storiesIndexed, <-ch)
+			}
 
-		for _, s := range storiesIndexed {
-			stories = append(stories, s.content)
+			sort.Sort(storiesIndexed)
+
+			for _, s := range storiesIndexed {
+				stories = append(stories, s.content)
+			}
+
+			log.Printf("Trying to cache stories...\n")
+
+			storiesJson, err := json.Marshal(stories)
+			if err != nil {
+				log.Println("Failed to cache top stories:", err)
+				http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
+				return
+			}
+			_, err = redisClient.Set(QuietHnResultsKey, string(storiesJson), 0).Result()
+			if err != nil {
+				log.Println("Failed to cache top stories:", err)
+				http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
+				return
+			}
+
+			log.Printf("Stories cached successfully.")
+		} else {
+			log.Printf("Using cache.\n")
+			if err = json.Unmarshal([]byte(storiesJson), &stories); err != nil {
+				log.Println("Failed to retrieve top stories from cache:", err)
+				http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
+				return
+			}
 		}
 
 		data := templateData{
